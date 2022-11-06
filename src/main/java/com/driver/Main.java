cd5aa64b0f100cc1fc8369096dea25e81f416785
[Bhanyprakash1706] <[mbhanuprakash1052000@gmail.com]>
2022-11-06 11:26:19 +0530

    Encapsulation is working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args){
+      RWOnly obj=new RWOnly();
+      //obj.name="Bhanu";
+      //java: name has private access in com.driver.RWOnly
+      obj.setName("Bhanu Prakash Reddy");
+      System.out.println(obj.getName());
+  }
 }
